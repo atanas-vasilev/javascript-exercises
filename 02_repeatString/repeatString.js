9da6a4688983d2c619075c5cf36437fc9bf19b88
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-07 11:22:38 +0000

    Completed repedString();

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    
+    if (times < 0) {
+        return 'ERROR';
+    }else if (times == 0) {
+        return '';
+    }else{
+        // return srt.repeat(times);
+        let newString = '';
+        for (let i = 0; i < times; i++) {
+            newString = newString+str;        
+        }
+        return newString
+    }
+    
 };
 
 // Do not edit below this line
