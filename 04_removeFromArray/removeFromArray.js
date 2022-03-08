0d84b74dcd1a1971484ff311ed85a4d6197bbcf1
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-08 07:36:11 +0000

    Completed .removeFromArray(); Using filter/includes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,20 @@
-const removeFromArray = (array, ...elementsToRemove) => {
+// const removeFromArray = (array, ...elementsToRemove) => {
 
-    elementsToRemove.forEach(
-        (elementToRemove) => {
-            for (let i = 0; i < array.length; i++) {
-                const element = array[i];
-                if (elementToRemove === element) {
-                    array.splice(i, 1);
-                }
-            }
-        }
-    )
-    return array;
-}
+//     elementsToRemove.forEach(
+//         (elementToRemove) => {
+//             for (let i = 0; i < array.length; i++) {
+//                 const element = array[i];
+//                 if (elementToRemove === element) {
+//                     array.splice(i, 1);
+//                 }
+//             }
+//         }
+//     )
+//     return array;
+// }
+
+const removeFromArray = (array, ...elementsToRemove) => array.filter(currentElement => !elementsToRemove.includes(currentElement))
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
