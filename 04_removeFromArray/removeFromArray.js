1c937fd5c6379e33d391fde21f018b7a267bbf4e
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-08 07:27:15 +0000

    Completed .removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...elementsToRemove) => {
 
-};
+    //for each element in arr, check if it matches any elements in elementsToRemove and if it does, remove it
+    elementsToRemove.forEach(
+        (elementToRemove) => {
+            for (let i = 0; i < array.length; i++) {
+                const element = array[i];
+                if (elementToRemove === element) {
+                    array.splice(i, 1);
+                }
+            }
+        }
+    )
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
