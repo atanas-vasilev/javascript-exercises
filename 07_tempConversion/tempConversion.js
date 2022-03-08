7e70a8ddd62a29b1a7b7c3a1c9f469d4a0822b11
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-08 18:15:50 +0000

    Completed tempConversion();

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //[°C] = ([°F] − 32) × 5⁄9
+  return Math.round((((Number(temp) - 32)*5)/9)*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Math.round((Number(temp)*1.8+32)*10)/10;
 };
 
 // Do not edit below this line
