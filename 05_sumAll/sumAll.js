a9fe08c95b742d5ffeac010f76d1b85b9f61a119
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-08 17:38:13 +0000

    Completed sumAll()

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (
+        num1 < 0 || num2 < 0 ||
+        typeof num1 != "number" || typeof num2 != "number"
+        ) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        let switcher = num2;
+        num2 = num1;
+        num1 = switcher;
+    }
+    
+    let sum = 0
+    
+    for (num1; num1 <= num2; num1++) {
+        sum = sum+num1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
